3436a39eed3ff76de4f7e39dafef88c9829bf2ce
Leonardo Eich <leo.eich@hotmail.com>
2023-08-21 22:55:23 -0300

    Insert user with post

--- src/main/java/com/leonardoeich/workshopmongo/services/UserService.java
+++ src/main/java/com/leonardoeich/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardoeich.workshopmongo.domain.User;
+import com.leonardoeich.workshopmongo.dto.UserDTO;
 import com.leonardoeich.workshopmongo.repositories.UserRepository;
 import com.leonardoeich.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDo) {
+		return new User(objDo.getId(), objDo.getName(), objDo.getEmail());
 	}
 
 }
